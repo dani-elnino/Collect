3ca75c90ba7c9ac6b0bd45b2dbd0a86eeec58d17
DanielNino <daniel.nino@ou.edu>
2020-11-04 17:03:10 -0600

    Use Set to find unique elements of a List

--- src/Collect.java
+++ src/Collect.java
@@ -1,4 +1,21 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class Collect {
+	public static void main(String[] args) {
+		//Fisrt part of Collect assignment.
+		//Use Set to find unique elements of a List.
+		List<String> list = Arrays.asList("dog", "dog", "cat", "duck", "pig");
+		System.out.println(list);
+		
+		Set<String> hashSet = new HashSet<String>(list);
+		System.out.println(hashSet);
 
+		 
+	}
+	
 }
